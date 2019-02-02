9d82b378b001a7548ca374f49cda2ee0980d2990
DaShinja <byron@dashinja.com>
2019-02-02 14:03:39 -0500

    proper html routing confirmed

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,3 +6,15 @@ const connection = mysql.createConnection({
   password: process.env.SQL_PW,
   database: 'friends_db'
 })
+
+// You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+
+let profileData = [
+  {
+    name: 'shinja',
+    photo: 'https://via.placeholder.com/200x200',
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+]
+
+module.exports = profileData
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 require('./app/routing/apiRoutes')(app)
