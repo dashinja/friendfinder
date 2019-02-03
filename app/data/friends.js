d66171d536da142bf5c5fb38af2af0ea18a92ae0
DaShinja <byron@dashinja.com>
2019-02-03 13:20:24 -0500

    save state #2

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,9 +13,23 @@ let profileData = [
   {
     name: 'shinja',
     photo: 'https://via.placeholder.com/200x200',
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
-    scoreSum: 32
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: 'Zach',
+    photo: 'https://some.photo.com/me.jpg',
+    scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+  },
+  {
+    name: 'Keisha',
+    photo: 'https://via.placeholder.com/200x200',
+    scores: [5, 4, 3, 2, 1, 2, 3, 4, 5, 6]
+  },
+  {
+    name: 'Halbanasta',
+    photo: 'https://via.placeholder.com/200x200',
+    scores: [2, 1, 3, 4, 5, 5, 2, 1, 4, 1]
   }
-]
+];
 
-module.exports = profileData
+module.exports = profileData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,45 @@
-let profiles = require('../data/friends')
+let profiles = require('../data/friends');
 // let questions = require('../data/questionPopulator')
 
 module.exports = function(app) {
   app.get('/api/friends', (req, res) => {
     // display JSON of all possible *friends*
 
-    res.json(profiles)
-  })
+    res.json(profiles);
+  });
   // app.get('/data/friends', (req, res) => {
   //   res.json(profiles)
   // })
   app.post('/api/friends', (req, res) => {
     // handle incoming servery results
     // also, compatability logic
-    console.log("I'm recieved from survey.html", req.body)
-    if (req.body) {
-      let sum = 0
-      req.body.scores.forEach(elem => {
-        sum += parseInt(elem)
-      })
+    console.log("I'm received from survey.html", req.body);
 
-      // console.log("I'm sum", sum)
-      req.body.scoreSum = sum
-      console.log("I'm score on req.body", req.body.scoreSum)
-      profiles.push(req.body)
+    let totalDifference;
+
+    // find diff between each element of array across arrays per question
+
+    let myResponseObject = {
+      errorCheck: true,
+      myData: profiles
+    };
+    res.json(myResponseObject);
+
+    if (req.body) {
+      profiles.push(req.body);
+      let diff;
+      let submitObj = req.body;
+      for (let i = 0; i < profiles.length; i++) {
+        diff = profiles[i].scores[i] - profiles[i + 1].scores[i];
+      }
+      console.log("I'm operated diff:", diff);
+      //   let sum = 0;
+      //   req.body.scores.forEach(elem => {
+      //     sum += parseInt(elem);
+      //   });
+      //   // console.log("I'm sum", sum)
+      //   req.body.scoreSum = sum;
+      //   console.log("I'm score on req.body", req.body.scoreSum);
       // function diff(arr) {
       //   let filtered = arr.filter(stuff => {
       //     console.log(profiles[stuff].name)
@@ -33,18 +49,12 @@ module.exports = function(app) {
       // }
       // let matcher = profiles.some(diff(profiles))
       // console.log("I'm matcher, did I win?", matcher)
-
-      let myResponseObject = {
-        errorCheck: true,
-        myData: profiles
-      }
-      res.json(myResponseObject)
     } else {
-      res.json(false)
+      res.json(false);
     }
 
     // res.redirect('/')
-  })
+  });
 
   // app.post('/data/friends', (req, res) => {
   //   // handle incoming servery results
@@ -62,4 +72,4 @@ module.exports = function(app) {
   //   console.log("I'm res.body:", res.body)
   //   res.json(questions)
   // })
-}
+};
