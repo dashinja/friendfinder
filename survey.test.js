4bfc050407a9012fbec9c3da7f975c58d65eecec
Byron <byron.miller@gmail.com>
2022-10-16 04:28:03 -0400

    Test setup works for non-dynamic DOM

---
+++ survey.test.js
@@ -0,0 +1,24 @@
+jest.dontMock('jquery')
+const $ = require('jquery')
+const { readFileSync } = require('fs')
+
+describe('Survey Page', () => {
+
+  beforeAll(() => {
+    var f = readFileSync('./app/public/survey.html', 'utf-8')
+    console.log('DOM as is: ', f)
+    document.documentElement.innerHTML = f
+
+    
+  })
+
+  test('body shows correctly', () => {
+    expect($('h1').hasClass('display-3')).not.toBe(true)
+    expect($('h1').hasClass('display-2')).toBe(true)
+
+    expect($('h3').hasClass('display-3')).not.toBe(true)
+    expect($('h3').hasClass('display-4')).toBe(true)
+
+  })
+
+})
